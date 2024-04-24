2e082aed9cf028b6c0772afe4f4d5c87f69e41ef
Jeff-desv <brunojeff0012@gmail.com>
2024-04-24 20:49:40 -0300

    atualização

--- java-anatomia-clases/src/MinhaClasse.java
+++ java-anatomia-clases/src/MinhaClasse.java
@@ -1,7 +1,17 @@
 public class MinhaClasse {
 
     public static void main (String [] args){
-        System.out.println("Hello World!!");
+       
+        String primeiroNome = "Jeffson";
+        String segundoNome = "Bruno";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método" +  primeiroNome.concat(" ").concat(segundoNome);
     }
     
 }
