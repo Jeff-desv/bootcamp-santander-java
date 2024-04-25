e7c5afce76011fdb38687cdb16570876024a709f
Jeff-desv <brunojeff0012@gmail.com>
2024-04-24 21:45:55 -0300

    Tipos e Variáveis

---
+++ Tipos-e-Variáveis/src/TiposVariaveis.java
@@ -0,0 +1,8 @@
+public class TiposVariaveis {
+    public static void main (String [] args){
+        double salarioMinimo = 2500;
+        int numeroNoral = 10;
+
+        short numeroCurto2 = (short) numeroNoral;
+    }
+}
