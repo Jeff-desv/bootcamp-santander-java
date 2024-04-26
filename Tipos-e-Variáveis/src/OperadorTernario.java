5e3660f7861958803761b215562dd42aa21fa069
Jeff-desv <brunojeff0012@gmail.com>
2024-04-25 21:13:26 -0300

    Atualizacao2

---
+++ Tipos-e-Variáveis/src/OperadorTernario.java
@@ -0,0 +1,25 @@
+public class OperadorTernario {
+    public static void main(String[] args) {
+        
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+       // String resultado = " ";
+
+        //if(a==b){
+       //     resultado = "Verdadeiro";
+       // } else {
+        //    resultado = "Falso";
+        //}
+
+        //System.out.println(resultado);
+
+        //Fazendo a mesma Lógica com Operador Ternário
+
+        String resultado = a == b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+    }
+}
---
+++ Tipos-e-Variáveis/src/incremento.java
@@ -0,0 +1,18 @@
+public class incremento {
+    public static void main(String[] args){
+
+        //incremento
+        int numero = 5;
+
+        numero++; //É a mesma coisa que numero + 1;
+
+        System.out.println(numero);//Resultado = 6
+
+        //decremento
+
+        numero--;// É a mesma coisa que numero -1;
+        System.out.println(numero);
+
+
+    }
+}
