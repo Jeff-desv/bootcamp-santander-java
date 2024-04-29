f982e3c972ed0171382a28a3152a5ca899328bc7
Jeff-desv <brunojeff0012@gmail.com>
2024-04-29 17:55:00 -0300

    Estrutura-de-Repeticao

---
+++ Estrururas-de-Repetição/src/App.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        /* ESTRUTURAS DE REPETIÇÃO
+
+        Laços de Repetição, também conhecidos como laços de itteração
+        ou simplesmente Loops, são comandos que permitem iteração de 
+        código, ou seja, que comandos presentes no bloco sejam repetidos
+        diversas vezes.
+    
+        Laços ou repetições são representados pelas seguintes estruturas:
+
+        FOR (para);
+        WHILE (enquanto);
+        DO WHILE (faça enquanto);
+         */
+
+        //-----FOR-----//
+        /*
+         * O comando FOR ("Para") permite que uma variável contadora seja testada
+         * e incrementada a cada iteração, sendo essas informações definidas na chamada 
+         * do comando. O comando FOR recebe como entrada, uma variável contadora, a condição
+         * e o valor de incrementação.
+         * 
+         * A estrutura de sintaxe do controle de repetição FOR é exibida abaixo:
+         * 
+         * for (bloco de inicialização; expressão booleana de validação; bloco de atualização){
+         *          //comando que será executado até que a
+         *          // expressão de validação torne-se falsa
+         * }
+         */
+    }
+}
