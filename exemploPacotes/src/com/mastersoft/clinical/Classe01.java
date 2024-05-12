75ae2b4d33631481507402c73d1277915e1ad62b
Jeff-desv <brunojeff0012@gmail.com>
2024-05-12 09:47:52 -0300

    tema-pacotes

---
+++ exemploPacotes/src/com/mastersoft/clinical/Classe01.java
@@ -0,0 +1,25 @@
+package com.mastersoft.clinical;
+
+public class Classe01 {
+
+	public static void main(String[] args) {
+		// Sobre os Pacotes em Java
+		/*
+		 * Os pacotes servem para organizar Classes
+		 * Um Pacote ou PACKAGE na tecnologia Java, nada mais é do que 
+		 * um conjunto de classes localizadas na mesma estrutura hierárquica de 
+		 * diretórios. Usualmente, são colocados em um PACKAGE classes relacionadas,
+		 * construídas com um propósito comum para promover a reutilização de código;
+		 * assim, sobre certos aspectos, os PACKAGES reproduzem a ideia das bibliotecas 
+		 * de código(libraries e units)
+		 */
+		/*
+		 * Na programação Java a reutilização de código pode ser apoiada pelaa construção
+		 * de pacotes de classes com métodos destinados a solucionar tarefas bastantes corriqueiras,
+		 * como por exemplo: Validação de CPF e CNPJ, operações com data, manipulação de vetores, 
+		 * cálculos matemáticos como médias e percentuais, entre outras.
+		 */
+
+	}
+
+}
---
+++ exemploPacotes/src/com/mastersoft/clinical/model/Cliente.java
@@ -0,0 +1,10 @@
+package com.mastersoft.clinical.model;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
