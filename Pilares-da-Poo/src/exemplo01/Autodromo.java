070e40f356d6cd5c41e4a2722258ef0984dd4c3b
Jeff-desv <brunojeff0012@gmail.com>
2024-05-18 20:02:40 -0300

    exemplo em POO

---
+++ Pilares-da-Poo/src/exemplo01/Autodromo.java
@@ -0,0 +1,8 @@
+package exemplo01;
+
+public class Autodromo {
+    public static void main(String[] args) {
+        Carro porsche = new Carro();
+        porsche.ligar();
+    }
+}
--- Pilares-da-Poo/src/exemplo01/Carro.java
+++ Pilares-da-Poo/src/exemplo01/Carro.java
@@ -1,5 +1,18 @@
 package exemplo01;
 
 public class Carro {
-    
+    public void ligar(){
+        confereCambil();
+        confereCombustivel();
+        System.out.println("Carro Ligado");
+    }
+
+    //Usando o Encapsulamento
+    private void confereCombustivel(){
+        System.out.println("Conferindo Combustível");
+    }
+
+    private void confereCambil(){
+        System.out.println("Conferindo se o Câmbio está em P");
+    }
 }
