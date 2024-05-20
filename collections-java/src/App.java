397d5b58c33b66457cb461b9317a305d905e8ade
Jeff-desv <brunojeff0012@gmail.com>
2024-05-20 20:49:50 -0300

    Collections em Java

---
+++ collections-java/src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Informação sobre as aulas");
+    }
+
+    public void info(){
+        /*
+         * Collection API
+         * 
+         * Uma coleção (Collection) é uma estrutura de dados que serve para 
+         * agrupar muitos elementos em uma única unidade, estes elementos precisarão
+         * ser Objetos.
+         * 
+         * Uma Collection pode ter coleções homogêneas e heterogêneas, normalmente utilizamos
+         * coleções homogêneas de um tipo específico.
+         * 
+         * O núcleo principal das coleções é formado pelas interfaces, e essas interfaces permitem
+         * manipular a coleção independente do nível de detalhe que elas representam.
+         * 
+         * Temos quatro grandes tipos de coleções: LIST(lista), SET(conjunto), QUEUE(fila) e MAP(mapa),
+         * a partir dessas interfaces, temos muitas subclasses concretas que implementam varias formas 
+         * diferentes de se trabalhar com cada coleção.
+         */
+    }
+}
