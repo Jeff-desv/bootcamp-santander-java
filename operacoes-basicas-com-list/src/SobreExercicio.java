0a316baacc3dabd210a0eadc61663178b67aa5d7
Jeff-desv <brunojeff0012@gmail.com>
2024-05-21 20:55:43 -0300

    Commit Inicial

---
+++ operacoes-basicas-com-list/src/SobreExercicio.java
@@ -0,0 +1,16 @@
+public class SobreExercicio {
+    public static void main(String[] args) throws Exception {
+        /*
+         * 1º Lista de Tarefas 
+         * 
+         * Crie uma Classe Chamada "ListaTarefas" que possui uma lista de tarefas como Atributo.
+         * Cada tarefa é representada por uma classe Chamada "Tarefa" que possui um atributo de descrição. 
+         * Implemente os seguintes métodos:
+         * 
+         * adcionarTarefa(String descricao) : Adciona uma nova tarefa à lista com a descrição fornecida.
+         * removerTarefa(String descricao) : Remove uma tarefa da lista com base em sua descricao.
+         * obterNumeroTotalTarefa() : Retorna o número total de tarefas na lista.
+         * obterDescricoesTarefas() : Retorna uma lista contendo a descrição de todas as tarefas na lista.
+         */
+    }
+}
