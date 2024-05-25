561611a4eaa9f2518cdf8c7b6e754b6b8d50ff48
Jeff-desv <brunojeff0012@gmail.com>
2024-05-25 08:24:16 -0300

    Atualizando  a Classe Tarefa

--- operacoes-basicas-com-list/src/operacoesBasicas/Tarefa.java
+++ operacoes-basicas-com-list/src/operacoesBasicas/Tarefa.java
@@ -1,5 +1,19 @@
 package operacoesBasicas;
 
+//Operações Basicas: LIST
 public class Tarefa {
+
+    //Criando Atributo
+    private String descricao;
+
+    //Criando o Constructor para receber a descrção
+    public Tarefa(String descricao){
+        this.descricao = descricao;
+    }
+
+    //Criando o método getter
+    public String getDescricao(){
+        return descricao;
+    }
     
 }
