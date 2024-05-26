fa3173b9e8fbfdc76b9079f3a9ecbcfa10f5afa4
Jeff-desv <brunojeff0012@gmail.com>
2024-05-26 12:09:19 -0300

    Pesquisa com List

---
+++ operacoes-basicas-com-list/src/pesquisa/Livro.java
@@ -0,0 +1,42 @@
+package pesquisa;
+
+public class Livro {
+
+    //Atributos
+    private String titulo;
+
+    private String autor;
+
+    private int anoPublicacao;
+
+    //Criando um Construtor para receber os atributos
+    public Livro(String titulo, String autor, int anoPublicacao){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    //Criando o Getter pois os atributos são privados
+    //O Getter vai servir para que possamos ter essas informações
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public String getAutor(){
+        return autor;
+    }
+
+    public int getAnoPublicacao(){
+        return anoPublicacao;
+    }
+    //Sobrescrevendo para toString 
+    //Isso será feito quando precisar imprimir uma lista
+    //No nosso caso uma lista de livros
+
+    public String toString(){
+        return "Livro";
+    }
+
+}
+
+
---
+++ operacoes-basicas-com-list/src/pesquisa/catalagoLivros.java
@@ -0,0 +1,31 @@
+package pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class catalagoLivros {
+    
+    //Criando Atributos
+    private List<Livro> livroList;
+
+    public catalagoLivros(){this.livroList = new ArrayList<>();}
+
+    //Criando o método adcionarLivros
+    public void adcionarLivro(String titulo, String autor, int anoPublicacao){
+
+    }
+
+    //Criando o método pesquisarPorAutor
+    public void pesquisarPorAutor(String autor){
+
+    }
+
+    //Criando o método pesquisarPorIntervalosAnos
+    public void pesquisarPorIntervalosAnos(int anoInicial, int anoFinal){
+
+    }
+    //Criando o método pesquisarPorTítulo
+    public void pesquisarPorTitulo(String titulo){
+        
+    }
+}
