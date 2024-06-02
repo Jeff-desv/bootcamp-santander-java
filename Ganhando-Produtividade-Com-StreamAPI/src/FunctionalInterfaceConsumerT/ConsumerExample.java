c87c4bd32a467a034f6e8ad926251cbb9afc63de
Jeff-desv <brunojeff0012@gmail.com>
2024-06-02 17:47:48 -0300

    Trabalhando com StreamAPI commit Inicial

---
+++ Ganhando-Produtividade-Com-StreamAPI/src/FunctionalInterfaceConsumerT/ConsumerExample.java
@@ -0,0 +1,34 @@
+package FunctionalInterfaceConsumerT;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+
+/*
+ * Representa uma Operação que aceita um Argumento do Tipo T e não retorna
+ * nenhum resultado.
+ * É utilizado princupalmente para realizar ações, ou efeitos colaterais do
+ * Stream sem modificar, ou retornar um valor.
+ * 
+ */
+public class ConsumerExample {
+
+    public static void main(String[] args) {
+        //Criar uma Lista com números inteiros
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        //Usando o Consumer com expressão Lambda para imprimir numeros Pares
+        Consumer<Integer> imprimirNumerosPar = numero -> {
+            if(numero % 2 == 0){
+                System.out.println(numero);
+            }
+        };
+
+        System.out.println();
+        //Usar o Consumer para imprimir números pares no Stream
+        numeros.stream().forEach(imprimirNumerosPar);
+
+        System.out.println();
+    }
+}
--- Operacoes-basicas-com-Map/src/Pesquisa/EstoqueProdutos.java
+++ Operacoes-basicas-com-Map/src/Pesquisa/EstoqueProdutos.java
@@ -23,13 +23,54 @@ public class EstoqueProdutos {
 
     public double calcularValorTotalEstoque(){
 
-        double valorTotalEstoque = 0;
+        double valorTotalEstoque=0;
         if(estoqueProdutoMap.isEmpty()){
             for(Produto p : estoqueProdutoMap.values()){
                 valorTotalEstoque += p.getQuantidade() * p.getPreco();
             }
         }
         return valorTotalEstoque;
+    }
+
+    public Produto obterProdutoMaisCaro(){
+        Produto produtoMaisCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
+        
+        if(!estoqueProdutoMap.isEmpty()){
+
+            for(Produto p : estoqueProdutoMap.values()){
+                if(p.getPreco() > maiorPreco){
+                    produtoMaisCaro = p;
+                }
+            }
+        }
+        return produtoMaisCaro;
+    }
+
+    public static void main(String[] args) {
+        EstoqueProdutos estoque = new EstoqueProdutos();
+
+        System.out.println();
+        estoque.exibirProduto();
+        System.out.println();
+
+        estoque.adcionarProduto(02l, "Produto 1", 10, 0.52);
+        estoque.adcionarProduto(1l, "Produto 2", 5, 1.50);
+        estoque.adcionarProduto(055l, "Produto 3", 100, 2.50);
+        estoque.adcionarProduto(004l, "Produto 4", 35, 5.0);
+        estoque.adcionarProduto(021l, "Produto 5", 10, 5.0);
+        estoque.adcionarProduto(010l, "Produto 6", 2, 10.0);
+        estoque.adcionarProduto(00l, "Produto 7", 15, 11.0);
+        estoque.adcionarProduto(111l, "Produto 8", 2, 15.0);
+
+        System.out.println();
+        estoque.exibirProduto();
+        System.out.println();
+
+        System.out.println("O valor Total do Estoque e: R$ " + estoque.calcularValorTotalEstoque());
+        System.out.println();
+        System.out.println("O Produto mais crao : R$ " + estoque.obterProdutoMaisCaro());
+        System.out.println();
     }
     
 }
